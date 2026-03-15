a458867b0dcac3a46187ccd7ec36f03f8e3ab921
Ahmamou abdelaziz <Abdelaziz.ahmamou@emsi-edu.ma>
2026-03-15 14:23:06 +0100

    added the type builtin command

--- main.c
+++ main.c
@@ -15,6 +15,14 @@ int main(int argc, char *argv[]) {
       break;
     }else if (strncmp(input, "echo ", 5) == 0) {
       printf("%s\n", input + 5);
+    } else if (strncmp(input, "type ", 5) == 0) {
+      // Extract the argument after "type " command
+      char *arg = input + 5;
+      // Check if the argument is a shell builtin
+      if (!strcmp(arg, "exit") || !strcmp(arg, "echo") || !strcmp(arg, "type"))
+        printf("%s is a shell builtin\n", arg);
+      else
+        printf("%s: not found\n", arg);
     } else {
       printf("%s: command not found\n", input);
     }
